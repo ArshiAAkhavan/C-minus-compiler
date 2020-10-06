488e78ba9185206a8d1e6b90d18aedb5e07aece9
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-06 17:39:51 +0330

    implementing Node and NoTokenMatchedException

---
+++ DFA.py
@@ -0,0 +1,23 @@
+class NoTokenMatchedError(Exception):
+    def __init__(self, token_lexeme):
+        super(NoTokenMatchedError, self).__init__(f"could not match lexeme[{token_lexeme}] with any known regular expressions...")
+
+class Node:
+    def __init__(self,action=None):
+        self.action=action
+        self.children=[]
+        self.current_token=""
+    
+    def append(self,edge,child):
+        self.children.append((edge,child))
+    
+    def match(self,char):
+        for (edge,child),_ in self.children:
+            if char in edge:
+                return child
+        try:
+            return self.action(self.current_token)
+        except TypeError:
+            raise NoTokenMatchedError
+class DFA:
+    pass
