a8fc6722df9399e786fccdf81b3a8551346264ac
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-06 14:24:44 +0330

    refactor touches!

--- buffer_reader.py
+++ buffer_reader.py
@@ -1,41 +1,44 @@
 class Buffer_reader:
-    
-    def __init__(self,path,buffer_size):
-        
-        self.buffer_size=buffer_size
-        self.buffer_pointer=0
-        self.buffer=[]
 
-        self.path=path
-        self.input_file=open(self.path, "r")
+    def __init__(self, path, buffer_size):
 
-        self.refill_buffer()        
+        self.buffer_size = buffer_size
+        self.buffer_pointer = 0
+        self.buffer = []
+
+        self.path = path
+        self.input_file = open(self.path, "r")
+
+        self.__refill_buffer()
 
     def get_next_char(self):
         if self.buffer_pointer == self.buffer_size:
-            self.refill_buffer()
+            self.__refill_buffer()
 
-        next_char=self.buffer[self.buffer_pointer]    
-        self.buffer_pointer+=1
-        
+        next_char = self.buffer[self.buffer_pointer]
+        self.buffer_pointer += 1
+
         return next_char
 
-    def refill_buffer(self):
+    def __refill_buffer(self):
         self.buffer = self.input_file.read(self.buffer_size)
-        self.buffer_pointer=0
+        self.buffer_pointer = 0
 
     def has_next(self):
         if self.buffer_pointer >= len(self.buffer):
             return False
-        elif self.buffer_pointer <self.buffer_size:
-             return True
+        elif self.buffer_pointer < self.buffer_size:
+            return True
         else:
-            self.refill_buffer()
+            try:
+                self.__refill_buffer()
+            except Exception:
+                return False
             return self.has_next()
 
 
-if __name__=="__main__":
-    b=Buffer_reader("input.txt",3)
-    for i in range(1,40,1):
+if __name__ == "__main__":
+    b = Buffer_reader("input.txt", 3)
+    while(b.has_next()):
         print(b.get_next_char()+"\n")
         print(len(b.buffer))
