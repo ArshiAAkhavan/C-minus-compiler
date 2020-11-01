3591ea0f09fe9cc29bd3604206faf49388cb6dde
ghshenavar <ghshenavar@gmail.com>
2020-11-01 19:13:47 +0330

    delete duplicate code, terminal list added

--- parser/grammer.py
+++ parser/grammer.py
@@ -3,16 +3,19 @@ class Terminal:
         self.name=name
         self.first=[name]
 
+
 class NoneTerminal(Terminal):
     def __init__(self,name , first=[], follow=[]):
         self.name=name
         self.first=first
         self.follow=follow
 
+
 class Rule:
     def __init__(self,left,right):
         self.left=left
         self.right=right
+
 
 class Grammer:
     def __init__(self,none_terminals,terminals):
@@ -44,11 +47,17 @@ class Grammer:
                 follow=line.split(" ")
                 nt=self.get_element_by_id(follow[0])
                 nt.follow=[self.get_element_by_id(e) for e in follow[1:]]
-        
 
     def get_element_by_id(self,name):
         for nt in self.none_terminals:
             if nt.name==name: return nt
         for t in self.terminals:
             if t.name==name: return t
-        
+
+
+def init_terminals():
+    terminals = [Terminal('$'), Terminal('ε'), Terminal('ID'), Terminal(';'), Terminal('['), Terminal('NUM'),
+                 Terminal(']'), Terminal('('), Terminal(')'), Terminal('int'), Terminal('void'), Terminal(','),
+                 Terminal('{'), Terminal('}'), Terminal('break'), Terminal('if'), Terminal('else'), Terminal('while'),
+                 Terminal('return'), Terminal('switch'), Terminal('case'), Terminal('default'), Terminal(':'),
+                 Terminal('='), Terminal('<'), Terminal('=='), Terminal('+'), Terminal('-'), Terminal('*')]
