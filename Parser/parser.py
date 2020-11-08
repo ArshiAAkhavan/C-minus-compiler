133799d7222cf2e47a99040108504d78710e66cb
Arshia Akhavan <letmemakenewone@gmail.com>
2020-11-08 18:50:07 +0330

    refactor

--- Parser/parser.py
+++ Parser/parser.py
@@ -17,14 +17,19 @@ class LL1:
         self.root = Node(self.grammar.rules[0].left.name)
 
     def create_parse_table(self):
-        for rule in self.grammar.rules:
-            for predict in rule.predict_set:
-                self.p_table[(rule.left.name, predict.name)] = [p.name for p in rule.right]
+        self.update_productions()
+        self.update_synchs()
 
+    def update_synchs(self):
         for nt in self.grammar.non_terminals:
             for item in nt.follow:
                 if (nt.name, item.name) not in self.p_table:
                     self.p_table[(nt.name, item.name)] = "synch"
+
+    def update_productions(self):
+        for rule in self.grammar.rules:
+            for predict in rule.predict_set:
+                self.p_table[(rule.left.name, predict.name)] = [p.name for p in rule.right]
 
     def add_error(self, error_root, error_type):
         if error_type.lower() == "missing":
@@ -40,43 +45,43 @@ class LL1:
     def generate_parse_tree(self):
         self.stack = [self.root]
         token = self.get_next_valid_token()
-        stack_top = None
+        statement = None
         try:
             while len(self.stack):
-                stack_top = self.get_next_valid_stack_top()
-                stack_top.token = token
-                if self.grammar.is_terminal(stack_top.name):  # terminal
-                    if stack_top.name != self.get_token_key(token):  # not matching
-                        self.add_error(stack_top, "missing")
-                        self.remove_top(stack_top)
+                statement = self.get_next_valid_statement()
+                statement.token = token
+                if self.grammar.is_terminal(statement.name):  # terminal
+                    if statement.name != self.get_token_key(token):  # not matching
+                        self.add_error(statement, "missing")
+                        self.remove_statement(statement)
                     elif len(self.stack):
                         token = self.get_next_valid_token()
                 else:  # none_terminal
-                    key = (stack_top.name, self.get_token_key(token))
+                    key = (statement.name, self.get_token_key(token))
                     if key in self.p_table and self.p_table[key] != "synch":
-                        self.update_stack(stack_top, key)
+                        self.update_stack(statement, key)
                     else:
-                        token = self.panic(stack_top, key, token)
+                        token = self.panic(statement, key, token)
         except NoTokenLeftException:
-            self.remove_top(stack_top)
-            [self.remove_top(g) for g in self.stack]
+            self.remove_statement(statement)
+            [self.remove_statement(g) for g in self.stack]
 
         return self.root
 
-    def update_stack(self, stack_top, key):
-        self.stack.extend([Node(g, parent=stack_top) for g in self.p_table[key]][::-1])
+    def update_stack(self, statement, key):
+        self.stack.extend([Node(g, parent=statement) for g in self.p_table[key]][::-1])
 
-    def panic(self, stack_top, key, token):
+    def panic(self, statement, key, token):
         while key not in self.p_table:
             self.add_error(token, "illegal")
             token = self.get_next_valid_token()
-            key = (stack_top.name, self.get_token_key(token))
+            key = (statement.name, self.get_token_key(token))
         if self.p_table[key] != 'synch':
-            self.update_stack(stack_top, key)
+            self.update_stack(statement, key)
             return token
 
-        self.add_error(stack_top, "missing")
-        self.remove_top(stack_top)
+        self.add_error(statement, "missing")
+        self.remove_statement(statement)
         return token
 
     def get_next_valid_token(self):
@@ -88,24 +93,37 @@ class LL1:
         except Exception:
             raise NoTokenLeftException()
 
-    def get_next_valid_stack_top(self):
-        stack_top = self.stack.pop()
-        while len(self.stack) and stack_top.name == "ε":
-            stack_top = self.stack.pop()
-        return stack_top
+    def get_next_valid_statement(self):
+        statement = self.stack.pop()
+        while len(self.stack) and statement.name == "ε":
+            statement = self.stack.pop()
+        return statement
 
     @staticmethod
     def get_token_key(token):
         return (token.lexeme, token.type.name)[token.type in [TokenType.NUM, TokenType.ID]]
 
     @staticmethod
-    def remove_top(stack_top):
-        if stack_top.parent:
-            children = list(stack_top.parent.children)
-            children.remove(stack_top)
-            stack_top.parent.children = tuple(children)
+    def remove_statement(statement):
+        if statement.parent:
+            children = list(statement.parent.children)
+            children.remove(statement)
+            statement.parent.children = tuple(children)
+
+    def export_syntax_error(self, path):
+        with open(path, 'w', encoding='utf-8') as f:
+            if not self.errors:
+                f.write("There is no syntax error.\n")
+            for line_no, error in self.errors:
+                f.write(f"#{line_no} : syntax error, {error}\n")
 
     def export_parse_tree(self, path):
+        self.reformat_tree()
+        with open(path, 'w', encoding='utf-8') as f:
+            for pre, _, node in RenderTree(self.root):
+                f.write("%s%s\n" % (pre, node.name))
+
+    def reformat_tree(self):
         for node in PreOrderIter(self.root):
             if node.name == "ε":
                 node.name = "epsilon"
@@ -116,13 +134,3 @@ class LL1:
                     node.name = f"({token_type}, {node.token.lexeme}) "
                 except:
                     pass
-        with open(path, 'w', encoding='utf-8') as f:
-            for pre, _, node in RenderTree(self.root):
-                f.write("%s%s\n" % (pre, node.name))
-
-    def export_syntax_error(self, path):
-        with open(path, 'w', encoding='utf-8') as f:
-            if not self.errors:
-                f.write("There is no syntax error.\n")
-            for line_no, error in self.errors:
-                f.write(f"#{line_no} : syntax error, {error}\n")
