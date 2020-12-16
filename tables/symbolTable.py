6e0dcb7dfec61275740b7afb8d3aef328fc790c6
ArshiA Akhavan <letmemakenewone@gmail.com>
2020-12-16 08:41:29 +0330

    adding ScopeStack class

--- tables/symbolTable.py
+++ tables/symbolTable.py
@@ -3,13 +3,58 @@ from scanner.tokens import Token, TokenType
 
 IDRecord = namedtuple('IDRecord', 'token element_type no_args type scope')
 
+class ScopeStack:
+    def __init__(self,parent=None):
+        self.stack=[]
+        self.parent=parent
+
+    def append(self,token,force=False):
+        if force:
+            return self.append(token)
+        id_record = self.get_IDrecord(token.lexeme)
+        if id_record:
+            return id_record
+        else:
+            return self.parent.get_IDrecord(token.lexeme)
+
+    def __append(self,token):
+        id_record=IDRecord(token,None,None,None,self)
+        self.stack.append(id_record)
+        return id_record
+
+    def get_IDrecord(self, lexeme):
+        for record in self.stack:
+            if record.token.lexeme == lexeme:
+                return lexeme
+        return None
 
 class __SymbolTable:
     keyword = ["if", "else", "void", "int", "while", "break", "switch", "default", "case", "return"]
 
     def __init__(self):
         self.scopes = []
-        self.new_stack()
+        self.scopes.append(ScopeStack())
+
+    def new_scope_stack(self):
+        self.scopes.append(ScopeStack(self.scopes[-1]))
+
+    def get_current_scope(self):
+        return self.scopes[-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def add_symbol(self, token, is_declaration=False):
         if token.lexeme in self.keyword:
@@ -17,23 +62,6 @@ class __SymbolTable:
         elif token.lexeme not in self.ids:
             self.ids.append(token.lexeme)
         return token
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     def __str__(self):
         s = ""
@@ -48,30 +76,3 @@ class __SymbolTable:
             if i < len(self.keyword + self.ids) - 1:
                 file.write("\n")
         file.close()
-=======
-        else:
-            return self.add_id(token, is_declaration)
-
-    def new_stack(self):
-        self.scopes.append([])
-
-    def add_id(self, token, is_declaration):
-        if is_declaration:
-            record = self.fetch(token, 1)
-            if record is None:
-                return self.add_record(token)
-            else:
-                return record.token
-
-    def fetch(self, token, depth=0):
-        if depth == 0: depth = len(self.scopes)
-
-        for scope in self.scopes.reverse():
-            record = self.fetch_in_scope(self, token)
-            if record is not None:return 
-
-        return None
-
-    def add_record(self, token):
-        record = IDRecord(token.lexeme, '', '', '', len(self.scope_stacks))
->>>>>>> Stashed changes
