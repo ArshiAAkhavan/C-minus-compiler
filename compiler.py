2a941ae9ae3ca7ca1efc8d63a8cd2d7aa64cf6eb
ArshiA Akhavan <letmemakenewone@gmail.com>
2020-12-18 13:08:12 +0330

    adding generate_parse_tree to compiler.py

--- compiler.py
+++ compiler.py
@@ -9,10 +9,10 @@ from tables import tables
 # Ghazal Shenavar 97101897
 
 parser = LL1(build_scanner("input.txt"), init_grammar(), CodeGen())
+parser.generate_parse_tree()
 parser.export_parse_tree("parse_tree.txt")
 parser.export_code("output.txt")
 parser.export_syntax_error("syntax_errors.txt")
-
 tables.get_error_table().export("lexical_errors.txt")
 # tables.get_symbol_table().export("symbol_table.txt")
 tables.get_token_table().export("tokens.txt")
